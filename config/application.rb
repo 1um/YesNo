b69120404cb61098b4074e58430ab5a410ec20c6
Your Name <you@example.com>
2015-03-18 18:16:56 +0200

    cdn

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module DaNet
   class Application < Rails::Application
     config.generators.assets = false
     config.generators.helper = false
+    config.assets.enabled = true
+    config.assets.paths << "#{Rails.root}/app/assets/fonts" 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
